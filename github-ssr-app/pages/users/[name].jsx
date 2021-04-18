4edc1f20814bb54f0a7b563b02144f3cc036f549
hsyoon <hsyoon@wemb.co.kr>
2021-04-18 23:56:02 +0900

    repository 추가

--- github-ssr-app/pages/users/[name].jsx
+++ github-ssr-app/pages/users/[name].jsx
@@ -1,26 +1,43 @@
 import fetch from 'isomorphic-unfetch';
 import Profile from '../../components/Profile';
+import css from 'styled-jsx/css';
+import Repository from '../../components/Repository';
 
-const name = ({user}) => {
+const style = css`
+  .user-contents-wrapper {
+    display: flex;
+    padding: 20px;
+  }
+`;
+
+const name = ({user, repos}) => {
   if(!user) {
     return null;
   }
   return (
-    <>
+    <div className="user-contents-wrapper">
       <Profile user={user} />
-    </>
+      <Repository user={user} repos={repos} />
+      <style jsx>{style}</style>
+    </div>
   );
 };
 
 export const getServerSideProps = async ({query}) => {
   const { name } = query;
   try{
-    const res = await fetch(`https://api.github.com/users/${name}`);
-    if(res.status === 200) {
-        const user = await res.json();
-        return { props: { user } };
+    let user;
+    let repos;
+
+    const userRes = await fetch(`https://api.github.com/users/${name}`);
+    if(userRes.status === 200) {
+      user = await userRes.json();
     }
-    return { props: {} };
+    const repoRes = await fetch(`https://api.github.com/users/${name}/repos?sort=updated&page=1&per_page=10`);
+    if(repoRes.status === 200) {
+      repos = await repoRes.json();
+    }
+    return { props: { user, repos } };
   } catch(e) {
       console.log(e);
       return { props: {} };
