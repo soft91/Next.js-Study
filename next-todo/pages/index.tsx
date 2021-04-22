a5baf4cf07d75d32bff4bf6da3d02d725a9ae6a7
hsyoon <hsyoon@wemb.co.kr>
2021-04-22 16:40:10 +0900

    Todolist 기본적인 Container 구성

--- next-todo/pages/index.tsx
+++ next-todo/pages/index.tsx
@@ -3,7 +3,8 @@ import TodoList from "../components/TodoList";
 import { TodoType } from "../types/todo";
 
 const todos: TodoType[] = [
-  {id: 1, text: "마트 가서 장보기", color: "red", checked: false}
+  {id: 1, text: "마트 가서 장보기", color: "red", checked: false},
+  {id: 2, text: "슈퍼 가서 아이스크림사기", color: "blue", checked: false}
 ]
  
 const index: NextPage = () => {
