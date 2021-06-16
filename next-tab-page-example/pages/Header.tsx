367f71f32e00fd1179f46dd9e76e09a67bb7d0c5
hsyoon <hsyoon@wemb.co.kr>
2021-06-16 23:17:06 +0900

    페이지를 이동 시 Class를 적용하는 예제

--- next-tab-page-example/pages/Header.tsx
+++ next-tab-page-example/pages/Header.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
   return(
     <div>
       <Link href="/Page1">
-        <a>Page1 이동</a>
+        <a className={router.pathname === "/Page1" ? "active" : ""}>Page1 이동</a>
       </Link>
       <Link href="/Page2">
-        <a>Page2 이동</a>
+        <a className={router.pathname === "/Page2" ? "active" : ""}>Page2 이동</a>
       </Link>
       <Link href="/Page3">
-        <a>Page3 이동</a>
+        <a className={router.pathname === "/Page3" ? "active" : ""}>Page3 이동</a>
       </Link>
+      <style jsx>{`
+        .active {
+          color: blue;
+        }
+      `}</style>
     </div>
   )
 }
