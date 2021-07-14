efde6fff6bb05a82522d66db79cf9a89353dfe04
hsyoon <soft4750@gmail.com>
2021-07-14 22:59:55 +0900

    react-export-excel example

---
+++ next-excel-example/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import ExcelButton from './components/ExcelButton'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExcelButton />
+    </div>
+  )
+}
