4d069777796b8957956e8dddbc6d5caf7344f762
soft4750 <soft4750@gmail.com>
2022-12-13 20:47:36 +0900

    commit

--- my-app/pages/tomato.jsx
+++ my-app/pages/tomato.jsx
@@ -1,14 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useRef } from "react";
 
 const Tomato = () => {
-  return(
+  const h2Ref = useRef();
+
+  const handleCopyClipBoard = async () => {
+    const text = h2Ref.current.innerText;
+    if (typeof window !== "undefined")
+      await navigator.clipboard.writeText(text);
+    alert("복사 성공!");
+  };
+
+  return (
     <div>
-      <h2>Link to 'main' Page</h2>
+      <h2 ref={h2Ref}>Link to 'main' Page</h2>
+      <button onClick={handleCopyClipBoard}>클립보드</button>
       <Link href="/">
         <a>Move to '/'</a>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Tomato;
