3ab83571ea222b541fc2c1d61e0abe145ef1b9fe
soft4750 <soft4750@gmail.com>
2022-12-14 22:53:43 +0900

    feat: Session Check Test

--- next-auth-example/pages/api/auth/[...nextauth].ts
+++ next-auth-example/pages/api/auth/[...nextauth].ts
@@ -50,7 +50,7 @@ const authOptions: NextAuthOptions = {
 		},
 		async session({ session, token, user }) {
 			// Send properties to the client, like an access_token from a provider.
-			session.accessToken = token.accessToken;
+			//session.accessToken = token.accessToken;
 			return session;
 		},
 	},
