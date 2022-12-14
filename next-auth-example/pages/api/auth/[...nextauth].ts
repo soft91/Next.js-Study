469e5b89723598bbb5fb1f53dab29108ec34df5a
soft4750 <soft4750@gmail.com>
2022-12-14 22:53:17 +0900

    feat: Session Check Test

--- next-auth-example/pages/api/auth/[...nextauth].ts
+++ next-auth-example/pages/api/auth/[...nextauth].ts
@@ -4,40 +4,56 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
-    // }),
-    CredentialsProvider({
-      name: "Credentials",
-      type: "credentials",
-      credentials: {},
-      authorize(credentials, req) {
-        const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
-        if (email !== "yoon@gmail.com" && password !== "1234") {
-          return null;
-        }
+	session: {
+		strategy: "jwt",
+		maxAge: 24 * 60 * 60,
+		updateAge: 2 * 24 * 60 * 60,
+	},
+	providers: [
+		// GithubProvider({
+		//   clientId: process.env.GITHUB_ID,
+		//   clientSecret: process.env.GITHUB_SECRET,
+		// }),
+		// GoogleProvider({
+		//   clientId: process.env.GOOGLE_ID,
+		//   clientSecret: process.env.GOOGLE_SECRET,
+		// }),
+		CredentialsProvider({
+			name: "Credentials",
+			type: "credentials",
+			credentials: {},
+			authorize(credentials, req) {
+				const { email, password } = credentials as {
+					email: string;
+					password: string;
+				};
+				if (email !== "yoon@gmail.com" && password !== "1234") {
+					return null;
+				}
 
-        return { id: "1234", name: "Yoon", email: "yoon@gmail.com" };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
-    signOut: "/auth/signout",
-  },
+				return { id: "1234", name: "Yoon", email: "yoon@gmail.com" };
+			},
+		}),
+	],
+	pages: {
+		signIn: "/auth/signin",
+		error: "/auth/error",
+		signOut: "/auth/signout",
+	},
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persist the OAuth access_token to the token right after signin
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token, user }) {
+			// Send properties to the client, like an access_token from a provider.
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
--- next-auth-example/pages/auth/signin.tsx
+++ next-auth-example/pages/auth/signin.tsx
@@ -1,56 +1,61 @@
 import { NextPage } from "next";
-import { FormEventHandler, useState } from "react";
-import { signIn } from "next-auth/react";
+import { FormEventHandler, useState, useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
 
 interface Props {}
 
 const Signin: NextPage = (props): JSX.Element => {
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-  });
+	const [userInfo, setUserInfo] = useState({
+		email: "",
+		password: "",
+	});
+	const { data: session } = useSession();
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    const res = await signIn("credentials", {
-      email: userInfo.email,
-      password: userInfo.password,
-      redirect: false,
-    });
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+		e.preventDefault();
+		const res = await signIn("credentials", {
+			email: userInfo.email,
+			password: userInfo.password,
+			redirect: false,
+		});
 
-    console.log(res);
-  };
+		console.log(res);
+	};
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <input
-          type="email"
-          placeholder="yoon@email.com"
-          value={userInfo.email}
-          onChange={(e) =>
-            setUserInfo((prev) => ({
-              ...prev,
-              email: e.target.value,
-            }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="*******"
-          value={userInfo.password}
-          onChange={(e) =>
-            setUserInfo((prev) => ({
-              ...prev,
-              password: e.target.value,
-            }))
-          }
-        />
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
+	useEffect(() => {
+		console.log(session);
+	}, [session]);
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<h1>Login</h1>
+				<input
+					type="email"
+					placeholder="yoon@email.com"
+					value={userInfo.email}
+					onChange={(e) =>
+						setUserInfo((prev) => ({
+							...prev,
+							email: e.target.value,
+						}))
+					}
+				/>
+				<input
+					type="password"
+					placeholder="*******"
+					value={userInfo.password}
+					onChange={(e) =>
+						setUserInfo((prev) => ({
+							...prev,
+							password: e.target.value,
+						}))
+					}
+				/>
+				<input type="submit" value="Login" />
+			</form>
+		</div>
+	);
 };
 
 export default Signin;
