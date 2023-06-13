c0b08a30080efaa7b7b2520db759bab864a29f44
soft4750 <soft4750@gmail.com>
2023-06-14 00:13:20 +0900

    feat: React-Quill을 통해 이미지 업로드

--- next-quill-exmaple/pages/components/Editor/index.tsx
+++ next-quill-exmaple/pages/components/Editor/index.tsx
@@ -41,11 +41,44 @@ const CustomQuill = styled(QuillNoSSRWrapper)`
 const Editor = ({
 	value,
 	onChange,
+	setImgFiles,
 }: {
 	value: string;
 	onChange: (e: string) => void;
+	setImgFiles: Dispatch<SetStateAction<string[]>>;
 }) => {
 	const quillRef = useRef<ReactQuill>(null);
+
+	const imageHandler = async () => {
+		const input = document.createElement("input") as any;
+
+		input.setAttribute("type", "file");
+		input.setAttribute("accept", "image/*");
+		input.setAttribute("multiple", "");
+		input.click();
+
+		input.addEventListener("change", async () => {
+			try {
+				if (input.files.length > 10) {
+					throw new Error("10개 이상 이미지를 등록할 수 없습니다.");
+				}
+
+				const res = await // api 통신을 위한 코드
+				const editor = quillRef.current?.getEditor();
+				const range = editor?.getSelection() as any;
+
+				res.data.data.map((item: { s3Key: string; url: string }) => {
+					const imgUrl = item.url;
+					setImgFiles((prev) => [...prev, item.s3Key]);
+
+					editor?.insertEmbed(range.index, "image", imgUrl);
+					editor?.setSelection(range.index + 1);
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		});
+	};
 
 	const modules = useMemo(
 		() => ({
@@ -57,6 +90,7 @@ const Editor = ({
 					[{ list: "ordered" }, { list: "bullet" }, { align: [] }],
 					["image"],
 				],
+				handlers: { image: imageHandler },
 			},
 			clipboard: {
 				matchVisual: false,
